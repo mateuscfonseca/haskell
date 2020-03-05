6affb5e6f8cfb2df41bbe34f55773cd1e81ba098
mateuscfonseca <mateuscfonseca@gmail.com>
2020-03-05 03:47:39 -0300

    desafios hacker hank

--- hacker.hs
+++ hacker.hs
@@ -9,9 +9,27 @@ f1 n (x : arr) = do
 f2 n = take n (repeat (take n (repeat [n])))
 f2' n = take n (repeat (f2 n))           	
 f2'' 0 = []
-f2'' n = [n % ] ++ (f2'' (n-1)) 
+f2'' n = [n - 1] ++ (f2'' (n-1)) 
+
+f3 [] = []
+f3 l = [last l] ++ f3 (init l) 
+
+f4 arr = foldr (+) 0 ([x | x <- arr, odd x])
+
+len :: [a] -> Int
+len lst = foldr (+) 0 ([1 | x <- lst]) 
+
+f5 arr = [(abs x) | x <- arr]
+
+factorial 0 = 1
+factorial 1 = 1
+factorial x = x * factorial (x-1)
+
 
 main :: IO()
-main = do 
-       nRepeat <- readLn :: IO Int
-       print (f2'' nRepeat)
+main = do
+    n <- readLn :: IO Int
+    inputdata <- getContents
+    let
+        numbers = map read (lines inputdata) :: [Double]
+    putStrLn . unlines $ (map show . f n) numbers
