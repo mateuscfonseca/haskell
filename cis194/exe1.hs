79a00369038f7386a2c1c5f21a5607174ceba96f
mateuscfonseca <mateuscfonseca@gmail.com>
2020-03-07 18:42:32 -0300

    exercio 1 aula 1 cis194

---
+++ cis194/exe1.hs
@@ -0,0 +1,28 @@
+-- def getDigit(n, p):
+--   if(p == 0):
+--     digit = n % 1 * 10
+--     return 0 if digit == 0 else Integer(digit)
+--   return getDigit(n/10, p - 1)
+
+--              start: 0
+
+import Data.Fixed
+
+getFirstDigit :: Integer -> Integer
+getFirstDigit number = number `mod` 10
+
+
+getDigitAt :: Integer -> Integer -> Integer
+getDigitAt number 0 = getFirstDigit number
+getDigitAt number position = getDigitAt (number `div` 10) (position - 1)
+
+toDigits :: Integer -> [Integer]
+toDigits 0      = []
+toDigits number =  toDigits (number `div` 10) ++ [(getFirstDigit number)]
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev number = reverse (toDigits number)
+
+main :: IO ()
+main = do
+  print (toDigitsRev 123)
