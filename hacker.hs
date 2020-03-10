71cade884cf40b0c75aa813ec1f161be093589c8
Mateus Fonseca <mateuscfonseca@gmail.com>
2020-03-10 17:22:46 -0300

    analise hanoi

--- hacker.hs
+++ hacker.hs
@@ -31,20 +31,3 @@ expand x = 1 + x + sum ([x**y / factorial y | (x, y) <- zip (take 8 (repeat x)) 
 f :: [Double] -> [Double]
 f (x:[]) = [expand x]
 f (x:xs) = [expand x] ++ f (tail xs)
-
-readDouble :: Double
-readDouble = readDouble :: Double
-
-getDouble = do readDouble
-
-getUserInput 0 = []
-getUserInput n = [getDouble]
---
---
--- -- main = do
--- --        n <- readLn :: IO Int
--- --        print (f (take n (cycle[getUserInput])))
---
--- main = print getUserInput
-call n = getUserInput n
-main = print 1
