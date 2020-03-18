138a7c819e26177d4dd647137072928c15c5f5a8
mateuscfonseca <mateuscfonseca@gmail.com>
2020-03-18 01:15:22 -0300

    Capitulo 3 - Real World Haskell

---
+++ tree.hs
@@ -0,0 +1,14 @@
+data Tree a = Node {
+          value  :: a
+          ,left  :: (Tree a)
+          ,right :: (Tree a)
+        }
+        | Empty
+          deriving (Show, Eq)
+
+type IntTree = (Tree Integer)
+type StrTree = (Tree String)
+
+-- sumt :: IntTree -> Integer
+sumt Empty  = 0
+sumt (Node value left right) = value + sumt left + sumt right
